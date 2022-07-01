6bcf5aa3c00004cbadd1626f5d0120861ac4b498
Diego <diegoferreira9338@gmail.com>
2022-07-01 20:45:42 -0300

    Fixing JSON loading.

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,30 +1,24 @@
 import '../css/style.css';
+import jsonData from '../../data/data.json';
 
 const calendar = document.querySelector('.calendar');
 const total = document.querySelector('.value');
 
-const fetchData = async () => {
-  const data = await fetch('../data/data.json');
-  const jsonData = await data.json();
-  return jsonData;
-};
-
-const insertData = async () => {
-  const data = await fetchData();
-
-  data.map((d) => {
+const insertDataInCalendar = async () => {
+  jsonData.forEach(({ amount, day }) => {
     const el = document.createElement('div');
     el.setAttribute('class', 'calendar_cell');
     el.innerHTML = `
-      <div class="calendar_graphic" style="height: ${Number(d.amount)}%;}"></div>
-      <p class="day">${d.day}</p>
+      <div class="calendar_graphic" style="height: ${Number(amount)}%;}"></div>
+      <p class="day">${day}</p>
     `;
-    return calendar.appendChild(el);
+
+    calendar.appendChild(el);
   });
 
-  const totalValue = data.reduce((acc, value) => acc + Number(value.amount), 0);
+  const totalValue = jsonData.reduce((acc, { amount }) => acc + Number(amount), 0);
 
   total.innerHTML = `$${totalValue}`;
 };
 
-insertData();
+insertDataInCalendar();
