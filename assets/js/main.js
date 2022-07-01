e78b8403819fc2bebc4356bbb46d3e877484c480
Diego <diegoferreira9338@gmail.com>
2022-07-01 19:44:41 -0300

    Model completion.

---
+++ assets/js/main.js
@@ -0,0 +1,28 @@
+const calendar = document.querySelector('.calendar');
+const total = document.querySelector('.value');
+
+const fetchData = async () => {
+  const data = await fetch('../../data.json');
+  const jsonData = await data.json();
+  return jsonData;
+};
+
+const insertData = async () => {
+  const data = await fetchData();
+
+  data.map((d) => {
+    const el = document.createElement('div');
+    el.setAttribute('class', 'calendar_cell');
+    el.innerHTML = `
+      <div class="calendar_graphic" style="height: ${Number(d.amount)}%;}"></div>
+      <p class="day">${d.day}</p>
+    `;
+    return calendar.appendChild(el);
+  });
+
+  const totalValue = data.reduce((acc, value) => acc + Number(value.amount), 0);
+
+  total.innerHTML = `$${totalValue}`;
+};
+
+insertData();
